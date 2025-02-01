fcf9ea6eb088912b4db833e7dca518689866814a
kaylabday14 <kaylabday14@gmail.com>
2025-01-31 23:11:15 -0500

    removing abstract

--- src/model/game_library.py
+++ src/model/game_library.py
@@ -2,9 +2,7 @@ from game import Game
 
 
 class GameLibrary():
-    def __init__(self):
-        # Creates a new instance for each game in the game library
-        self.games = [
+    games = [
             Game('Chess', 'Board', 'Photo'), # Replace photo with actual file name in resources/images folder path
             Game('Poker', 'Card', 'Photo'),
             Game('Monopoly', 'Board', 'Photo')

90243a6f107fac8b92d48a1109ad1b3107989fdb
AyeeitsRo <164427862+AyeeitsRo@users.noreply.github.com>
2025-02-07 18:27:58 -0600

    Updating order.py in test folder

--- tests/model/order.py
+++ tests/model/order.py
@@ -1,32 +1,85 @@
-from abc import abstractmethod
 
-class Order:
-    def __init__(self, items, price, quantity, total, photo):
-        self.items = {
-            
-        }
-        self.price = price
-        self.quantity = quantity
-        self.total = total
-        self.photo = photo
-    
-    @property
-    @abstractmethod
-    def add_total(self, price, total):
-        """Property abstract method, will be implemented by child classes"""
-        pass
-    
-    def add_cart(self):
-        # Allow for adding multiple, deleting, then submit to back end
-        pass
-    
-    def __str__(self):
-        return f'Item: {self.item}, Price: {self.price}'
-    
+SALES_TAX = 1.07
 
 class FoodItem:
-    pass
+    def __init__(self, name: str, price: float, photo = None):
+        self.name = name
+        self.price = price
+        self.photo = photo
+        self.quantity: int = 1
+    
+    def __str__(self) -> str:
+        return f'Food: {self.name}, Price: {self.price:.2f}, QTY: {self.quantity}'
 
 
 class DrinkItem:
-    pass
+    def __init__(self, name: str, price: float, photo = None):
+        self.name = name
+        self.price = price
+        self.photo = photo
+        self.quantity: int = 1
+    
+    def __str__(self) -> str:
+        return f'Drink: {self.name}, Price: {self.price:.2f}, QTY: {self.quantity}'
+
+
+class Order:
+    def __init__(self):
+        self.items: list = [] # Holds list of user order
+        self.total: float = 0.0 
+
+    @property
+    def add_total(self) -> float:
+        """Calculates the total price of user order"""
+        for item in self.items:
+            total: float = item.price * item.quantity
+        taxed_total: float = total * SALES_TAX
+        return f"{taxed_total:.2f}"
+    
+    def add_item(self, item, quantity = 1) -> None:
+        """Add an item to user order"""
+        item.quantity = quantity
+        self.items.append(item)
+        self.total = self.add_total
+        
+    def remove_item(self, item) -> None:
+        """Remove an item from user order"""
+        self.items.remove(item)
+        self.total = self.add_total
+        
+    def clear_order(self) -> None:
+        """Clears all items from user order"""
+        self.items = []
+        self.total = 0.0
+    
+    def __str__(self) -> str:
+        """Returns str of items"""
+        item_details = "\n".join(str(item) for item in self.items)
+        return f'Items in order: \n{item_details}\n\nTotal: {self.total}'
+
+
+# Drinks the cafe sells
+BLACK_COFFEE = DrinkItem('Black Coffee', 3.00)
+LATTE = DrinkItem('Latte', 8.50)
+MOCHA = DrinkItem('Mocha', 8.50)
+CAPPUCCINO = DrinkItem('Capppuccino', 7.00)
+ORANGE_JUICE = DrinkItem('Orange Juice', 5.50)
+
+# Food the cafe sells
+CAKE_POP = FoodItem('Cake Pop', 3.50)
+CHOC_CROISSANT = FoodItem('Chocolate Croissant', 7.25)
+COFFEE_CAKE = FoodItem('Cinnamon Coffee Cake', 6.75)
+EVERYTHING_BAGEL = FoodItem('Everything Bagel', 7.00)
+CHOC_CHIP_COOKIE = FoodItem('Chocolate Chip Cookie', 3.00)
+
+
+order = Order()
+
+"""
+Example below of order output along with total
+**RUN CODE TO SEE OUTPUT**
+"""
+order.add_item(LATTE, quantity=1)
+order.add_item(COFFEE_CAKE, quantity = 2)
+
+print(order)
